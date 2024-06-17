e857ff9d564cc4cbba3ff45698992e57562beb84
Noel-S <noel.silva.v@gmail.com>
2024-06-17 22:13:30 +0000

    chore: Update import paths for ToggleGroup and ThemeProvider components

--- editor/src/App.tsx
+++ editor/src/App.tsx
@@ -1,11 +1,11 @@
 // import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
-import { ToggleGroup, ToggleGroupItem } from '@radix-ui/react-toggle-group'
 import './App.css'
+import { ToggleGroup, ToggleGroupItem } from '@/components/ui/toggle-group'
 import { ResizableHandle, ResizablePanel, ResizablePanelGroup } from '@/components/ui/resizable'
 import { FontBoldIcon, FontItalicIcon, UnderlineIcon } from '@radix-ui/react-icons'
-import { ThemeProvider } from './components/theme-provider'
+import { ThemeProvider } from '@/components/theme-provider'
 import CodeEditor from '@uiw/react-textarea-code-editor'
 // import markdownit from 'markdown-it'
 import { useRef } from 'react'
@@ -36,19 +36,17 @@ function App() {
                 fontFamily: 'ui-monospace,SFMono-Regular,SF Mono,Consolas,Liberation Mono,Menlo,monospace',
               }}
             />
-            <ToggleGroup type="single" onSelect={(e) => {
-          console.log(e)
-        }}>
-          <ToggleGroupItem value="bold" aria-label="Toggle bold">
-            <FontBoldIcon className="h-4 w-4" />
-          </ToggleGroupItem>
-          <ToggleGroupItem value="italic" aria-label="Toggle italic">
-            <FontItalicIcon className="h-4 w-4" />
-          </ToggleGroupItem>
-          <ToggleGroupItem value="strikethrough" aria-label="Toggle strikethrough">
-            <UnderlineIcon className="h-4 w-4" />
-          </ToggleGroupItem>
-        </ToggleGroup>
+            <ToggleGroup type="single" onValueChange={console.log} className="p-2" >
+              <ToggleGroupItem value="bold" aria-label="Toggle bold">
+                <FontBoldIcon className="h-4 w-4" />
+              </ToggleGroupItem>
+              <ToggleGroupItem value="italic" aria-label="Toggle italic">
+                <FontItalicIcon className="h-4 w-4" />
+              </ToggleGroupItem>
+              <ToggleGroupItem value="strikethrough" aria-label="Toggle strikethrough">
+                <UnderlineIcon className="h-4 w-4" />
+              </ToggleGroupItem>
+            </ToggleGroup>
             </section>
           </ResizablePanel>
           <ResizableHandle withHandle />
