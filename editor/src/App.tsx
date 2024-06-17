1ec42eef1f26b47f9ad217986be1d0aad67331f6
Noel-S <noel.silva.v@gmail.com>
2024-06-17 22:15:59 +0000

    chore: Remove unused imports and update import paths

--- editor/src/App.tsx
+++ editor/src/App.tsx
@@ -1,6 +1,3 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
 import './App.css'
 import { ToggleGroup, ToggleGroupItem } from '@/components/ui/toggle-group'
 import { ResizableHandle, ResizablePanel, ResizablePanelGroup } from '@/components/ui/resizable'
