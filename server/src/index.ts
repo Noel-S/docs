f9a5b015215272096e53a3d1799e2d8dc5ba5f8b
Noel-S <noel.silva.v@gmail.com>
2024-07-06 17:34:56 -0600

    chore: Update serveStatic paths and add support for editing Markdown files

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,9 +3,14 @@ import { serveStatic } from 'hono/bun'
 
 const app = new Hono()
 
-app.use('/editor/*', serveStatic({ root: '../editor/dist', rewriteRequestPath(path) {
+app.use('/edit/*', serveStatic({ root: '../editor/dist', rewriteRequestPath(path) {
   return path.replace(/^\/editor/, '')
 }, }))
+
+app.get('/edit/:filename{.+\\.md$}', serveStatic({ root: '../editor/dist', path: '/index.html', rewriteRequestPath(path) {
+  console.log('path', path)
+  return path.replace(/^\/editor/, '')
+}}))
 
 app.use('/*', serveStatic({ root: '../base/dist' }))
 
