40906e78f7a584d146a40fef23b7209337261230
Noel-S <noel.silva.v@gmail.com>
2024-07-06 23:11:39 -0600

    chore: Update serveStatic paths and add support for asve retrieve and publish endpoints

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,17 +1,49 @@
 import { Hono } from 'hono'
 import { serveStatic } from 'hono/bun'
+import { unlink } from "node:fs/promises";
 
 const app = new Hono()
 
 app.use('/edit/*', serveStatic({ root: '../editor/dist', rewriteRequestPath(path) {
-  return path.replace(/^\/editor/, '')
+  return path.replace(/^\/edit/, '')
 }, }))
 
-app.get('/edit/:filename{.+\\.md$}', serveStatic({ root: '../editor/dist', path: '/index.html', rewriteRequestPath(path) {
-  console.log('path', path)
-  return path.replace(/^\/editor/, '')
+app.get('/edit/:filename{.+\\.mdx{0,1}$}', serveStatic({ root: '../editor/dist', path: '/index.html', rewriteRequestPath(path) {
+  return path.replace(/^\/edit/, '')
 }}))
 
 app.use('/*', serveStatic({ root: '../base/dist' }))
+
+app.get('/content/:filename{.+\\.mdx{0,1}$}', async (c) => {
+  const { filename } = c.req.param()
+  const content = await Bun.file(`../base/${filename}`).text()
+  return c.text(content)
+})
+
+app.post('/save', async (c) => {
+  try {
+    const { old_file, new_file, move, content } = await c.req.json()
+    await Bun.write(`../base/${new_file}`, content)
+    if (move) {
+      await unlink(`../base/${old_file}`)
+    }
+    return c.json({ status: 'ok' })
+  } catch (error: any) {
+    return c.json({ status: 'error', error: error.message })
+  }
+})
+
+app.post('/publish', async (c) => {
+  try {
+    const { stderr } = Bun.spawn(['npm', 'run', 'astro', 'build'], { cwd: '../base' })
+    if (stderr) {
+      return c.json({ status: 'error' })
+    }
+    console.log('published')
+    return c.json({ status: 'ok' })
+  } catch (error: any) {
+    return c.json({ status: 'error', error: error.message })
+  }
+})
 
 export default app
