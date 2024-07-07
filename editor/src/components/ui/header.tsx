f7327b04a203a2a9ddca1b23cb940ec7a0aceff1
Noel-S <noel.silva.v@gmail.com>
2024-07-07 11:58:47 -0600

    chore: Add dialog component for file details editing and deleting

--- editor/src/components/ui/header.tsx
+++ editor/src/components/ui/header.tsx
@@ -2,12 +2,17 @@ import { Button } from './button'
 import { CopyIcon, DotsHorizontalIcon, GlobeIcon, Pencil2Icon, PlusIcon, TrashIcon } from '@radix-ui/react-icons'
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuSeparator, DropdownMenuTrigger } from './dropdown-menu'
 import { Breadcrumb, BreadcrumbEllipsis, BreadcrumbItem, BreadcrumbList, BreadcrumbPage, BreadcrumbSeparator } from './breadcrumb'
+import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from './dialog'
+import { DialogClose } from '@radix-ui/react-dialog'
+import { Input } from './input'
+import { Label } from './label'
+import { ToggleGroup, ToggleGroupItem } from './toggle-group'
+import { Separator } from './separator'
 
 export default function Header() {
   return (
     <header className='flex items-center justify-between p-3'>
       <div className='flex items-center gap-2'>
-        {/* <h1 className='text-xl'>Editor</h1> */}
         <Breadcrumb>
           <BreadcrumbList>
             <BreadcrumbItem>
@@ -27,9 +32,56 @@ export default function Header() {
             </BreadcrumbItem>
           </BreadcrumbList>
         </Breadcrumb>
-        <Button variant="secondary" size="icon">
-          <Pencil2Icon className="h-4 w-4" />
-        </Button>
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button variant="secondary" size="icon">
+              <Pencil2Icon className="h-4 w-4" />
+            </Button>
+          </DialogTrigger>
+          <DialogContent>
+            <DialogHeader>
+              <DialogTitle>File details</DialogTitle>              
+            </DialogHeader>
+            <DialogDescription className='flex flex-col gap-2'>
+              <div className="grid w-full items-center gap-2">
+                <Label htmlFor="parentfolder">Parent folder</Label>
+                <div className='flex w-full items-center gap-2'>
+                  <Input type="text" id="parentfolder" placeholder="src/content/docs/how-to" disabled />
+                  <Button variant="secondary" size="icon">
+                    <Pencil2Icon className="h-4 w-4" />
+                  </Button>
+                </div>
+              </div>
+              <div className="grid w-full items-center gap-2">
+                <Label htmlFor="filename">File name</Label>
+                <div className='flex w-full items-center gap-2'>
+                  <Input type="text" id="filename" placeholder="file" defaultValue={'filename'} />
+                  <ToggleGroup variant="outline" type="single" defaultValue='md'>
+                    <ToggleGroupItem value="md" aria-label="Toggle md">
+                      .md
+                    </ToggleGroupItem>
+                    <ToggleGroupItem value="mdx" aria-label="Toggle mdx">
+                      .mdx
+                    </ToggleGroupItem>
+                  </ToggleGroup>
+                </div>
+              </div>
+            </DialogDescription>
+            <Separator />
+            <DialogFooter>
+              <DialogClose asChild>
+                <Button type="button" variant="secondary">
+                  Close
+                </Button>
+              </DialogClose>
+              <DialogClose asChild>
+                <Button type="button" variant="default">
+                  Confirm
+                </Button>
+              </DialogClose>
+            </DialogFooter>
+          </DialogContent>
+        </Dialog>
         <Button variant="secondary" size="icon">
           <CopyIcon className="h-4 w-4" />
         </Button>
@@ -54,10 +106,34 @@ export default function Header() {
               <span>Publish</span>
             </DropdownMenuItem>
             <DropdownMenuSeparator />
-            <DropdownMenuItem className='text-red-400'>
-              <TrashIcon className="mr-2 h-4 w-4" />
-              <span>Delete</span>
-            </DropdownMenuItem>
+            <Dialog>
+              <DialogTrigger className='w-full'>
+                <DropdownMenuItem className='text-red-400' onSelect={(e) => e.preventDefault()}>
+                  <TrashIcon className="mr-2 h-4 w-4" />
+                  <span>Delete</span>
+                </DropdownMenuItem>
+              </DialogTrigger>
+              <DialogContent>
+                <DialogHeader>
+                  <DialogTitle>Are you absolutely sure?</DialogTitle>
+                  <DialogDescription>
+                    This action cannot be undone. This will permanently delete this file from the server.
+                  </DialogDescription>
+                </DialogHeader>
+                <DialogFooter>
+                  <DialogClose asChild>
+                    <Button type="button" variant="secondary">
+                      Close
+                    </Button>
+                  </DialogClose>
+                  <DialogClose asChild>
+                    <Button type="button" variant="destructive">
+                      Delete
+                    </Button>
+                  </DialogClose>
+                </DialogFooter>
+              </DialogContent>
+            </Dialog>
           </DropdownMenuContent>
         </DropdownMenu>
       </div>
