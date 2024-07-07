f6b59d4dcd991311fdd10168ab821c650690f366
Noel-S <noel.silva.v@gmail.com>
2024-07-06 23:10:57 -0600

    chore: Update CodeMirror and related dependencies for improved functionality and performance

--- editor/src/App.tsx
+++ editor/src/App.tsx
@@ -3,8 +3,11 @@ import { ToggleGroup, ToggleGroupItem } from '@/components/ui/toggle-group'
 import { ResizableHandle, ResizablePanel, ResizablePanelGroup } from '@/components/ui/resizable'
 import { FontBoldIcon, FontItalicIcon, UnderlineIcon } from '@radix-ui/react-icons'
 import { ThemeProvider } from '@/components/theme-provider'
-import CodeEditor from '@uiw/react-textarea-code-editor'
-import { useMemo, useRef } from 'react'
+import CodeMirror, { ReactCodeMirrorRef } from '@uiw/react-codemirror'
+import { tokyoNight } from "@uiw/codemirror-theme-tokyo-night"
+// import { markdown } from '@codemirror/lang-markdown';
+import { langs } from '@uiw/codemirror-extensions-langs'
+import { useEffect, useMemo, useRef } from 'react'
 import { remark } from 'remark'
 import remarkGfm from 'remark-gfm'
 import remarkSmartypants from 'remark-smartypants'
@@ -13,35 +16,52 @@ import { rehype } from 'rehype'
 import rehypeShiki from '@shikijs/rehype'
 
 function App() {
+  const pathRef = useRef<string|null>(null)
+  const markdownRef = useRef<ReactCodeMirrorRef>(null)
   const htmlRef = useRef<HTMLDivElement>(null)
-  const parser = useMemo(() => {
+  const markdownparser = useMemo(() => {
     return remark().use(remarkGfm).use(remarkSmartypants).use(remarkHtml)
   }, [])
+
   const htmlparser = useMemo(() => {
     return rehype().use(rehypeShiki, { theme: 'tokyo-night' })
   }, [])
+
+  useEffect(() => {
+    if (!markdownRef.current) return
+    if (!htmlRef.current) return
+    if (!htmlparser) return
+    if (!markdownparser) return
+    if (!window) return
+
+    const pathname = window.location.pathname
+    pathRef.current = pathname.replace(/^\/edit/, '')
+    // console.log('pathname', pathRef.current)
+    if (pathRef.current.endsWith('md') || pathRef.current.endsWith('mdx')) {
+      fetch(`/content${pathRef.current}`).then((res) => res.text()).then((insert) => {
+        markdownRef.current!.view?.dispatch({ changes: { from: 0, to: 0, insert }})
+      })
+    }
+  }, [htmlparser, markdownparser])
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <main className='main'>
         <ResizablePanelGroup direction="horizontal">
           <ResizablePanel minSize={30}>
-            <section className='flex flex-col h-full'>
-            <CodeEditor
-              language="markdown"
-              placeholder="Please enter JS code."
-              onChange={(evn) => {
-                parser.process(evn.target.value).then((file) => {
+            <section className='flex flex-col h-full' >
+            {/* <div id="editor" ref={markdownRef} ></div> */}
+            <CodeMirror 
+              height="100%" 
+              style={{ height: '100%', maxHeight: '100%', overflow: 'auto' }}
+              ref={markdownRef}
+              extensions={[langs.markdown()]} 
+              theme={tokyoNight}
+              onChange={(value) => {
+                markdownparser.process(value).then((file) => {
                   htmlparser.process(file).then((file) => {
                     htmlRef.current!.innerHTML = String(file)
                   })
                 })
-              }}
-              padding={15}
-              style={{
-                height: '100%',
-                maxHeight: '100%',
-                overflow: 'auto',
-                fontFamily: 'ui-monospace,SFMono-Regular,SF Mono,Consolas,Liberation Mono,Menlo,monospace',
               }}
             />
             <ToggleGroup type="single" onValueChange={console.log} className="p-2" >
